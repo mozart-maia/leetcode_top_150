ecf4162fc0abc20fb716eecf78a8ff986db4e6a7
Mozart Maia <mozartmaia89@gmail.com>
2024-10-22 22:20:04 -0300

    feat: 14

---
+++ 14/main.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+        first = strs[0]
+        for i in range(len(first)):
+            for s in strs:
+                if i == len(s) or s[i] != first[i]:
+                    return res
+            res += first[i]
+
+        return res
