98ab134f2956cb070b5682fd144d29469ba1f923
Mozart Maia <mozartmaia89@gmail.com>
2025-07-24 16:37:15 -0300

    fix: 1566

--- beecrowd/1566/main.c
+++ beecrowd/1566/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int comp(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
 
 int main(){
     int casos = 0;
@@ -12,6 +15,7 @@ int main(){
         int *arr_h;
         arr_h = (int *)malloc(habitantes * sizeof(int));
         if (arr_h == NULL){
+            printf("Erro no malloc!\n");
             return -1;
         }
         for(int j=0; j<habitantes;++j){
@@ -19,12 +23,21 @@ int main(){
             scanf("%d", &h);
             arr_h[j] = h;
         }
-        printf("\nPrinting array readed: ");
-        for(int j=0; j<habitantes;++j){
-            printf("%d ", arr_h[j]);
-            
+
+        // printf("\nPrinting array readed: ");
+        // for(int j=0; j<habitantes;++j){
+            // printf("%d ", arr_h[j]);            
+        // }
+        // printf("\n");
+
+        qsort(arr_h, habitantes, sizeof(arr_h[0]), comp);
+        
+        // printf("\nPrinting array sorted: ");
+
+        for(int j=0; j<habitantes-1;++j){
+            printf("%d ", arr_h[j]);            
         }
-        printf("\n");
+        printf("%d\n", arr_h[habitantes-1]);
     }
 
     return 0;
